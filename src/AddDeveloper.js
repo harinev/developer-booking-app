d07ca408b5ef7170b545f86cce5257eb0d130ff1
Harriet Ryder <harriethryder@gmail.com>
2019-12-20 19:17:59 +0000

    Add New Developer works and developers have UUIDs

--- src/AddDeveloper.js
+++ src/AddDeveloper.js
@@ -1,17 +1,44 @@
 import React from "react";
 
+// Controlled component
+// Component (with Form elements) where we are keeping track on the state of what values have been inputted by the user
 class AddDeveloper extends React.Component {
   state = {
     developerName: "",
-    skills: [],
+    skills: "",
     dateJoined: "2019-12-09"
   };
 
+  // Event handler
   updateDeveloperName = event => {
-    console.log(event.target.value);
     this.setState({
       developerName: event.target.value
     });
+  };
+
+  // Event handler
+  updateDeveloperSkills = event => {
+    this.setState({
+      skills: event.target.value
+    });
+  };
+
+  // Event handler
+  updateDate = event => {
+    this.setState({
+      dateJoined: event.target.value
+    });
+  };
+
+  // Event handler
+  handleAdd = () => {
+    const skillsArray = this.state.skills.split(", ");
+
+    this.props.addNewDevFunc(
+      this.state.developerName,
+      skillsArray,
+      this.state.dateJoined
+    );
   };
 
   render() {
@@ -29,15 +56,24 @@ class AddDeveloper extends React.Component {
         <div className="col-4">
           <input
             type="text"
+            onChange={this.updateDeveloperSkills}
+            value={this.state.skills}
             className="form-control"
             placeholder="Enter skills"
           />
         </div>
         <div className="col-3">
-          <input type="date" className="form-control" />
+          <input
+            type="date"
+            className="form-control"
+            value={this.state.dateJoined}
+            onChange={this.updateDate}
+          />
         </div>
         <div className="col-2">
-          <button className="btn btn-primary">Add</button>
+          <button onClick={this.handleAdd} className="btn btn-primary">
+            Add
+          </button>
         </div>
       </div>
     );
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import uuid from "uuid/v4";
 import DeveloperCount from "./DeveloperCount";
 import Header from "./Header";
 import Developer from "./Developer";
@@ -16,38 +17,64 @@ class App extends React.Component {
         skills: ["TDD", "Debugging"],
         available: false,
         dateJoined: "2019-12-02",
-        id: 1
+        id: uuid()
       },
       {
         name: "Fiona Castillo",
         skills: ["HTML", "CSS"],
         available: true,
         dateJoined: "2019-11-30",
-        id: 2
+        id: uuid()
       },
       {
         name: "Harine Vijay",
         skills: ["Java"],
         available: false,
         dateJoined: "2019-12-01",
-        id: 3
+        id: uuid()
       },
       {
         name: "Ilga Koko",
         skills: ["HTML", "TDD", "React"],
         available: true,
         dateJoined: "2019-10-22",
-        id: 4
+        id: uuid()
       },
       {
         name: "Nichola Evans",
         skills: ["CSS", "Ruby", "Python"],
         available: false,
         dateJoined: "2019-12-09",
-        id: 5
+        id: uuid()
       }
     ]
   };
+
+  // ONE RULE
+  // Any function that updates state MUST live in the same component as the state
+  addNewDeveloper = (name, skills, date) => {
+    // Create a new developer object
+    const newDeveloper = {
+      name: name,
+      skills: skills,
+      available: true,
+      dateJoined: date,
+      id: uuid()
+    };
+
+    // Add that developer to the list
+    // ANOTHER RULE
+    // You must never update state directly, setState is the correct way of updating state
+    // Slice makes a copy of an array
+    const copyOfDevs = this.state.developers.slice();
+    copyOfDevs.push(newDeveloper);
+
+    // Ensure the state is updated (setState)
+    this.setState({
+      developers: copyOfDevs
+    });
+  };
+
   render() {
     const availableDevelopers = this.state.developers.filter(developer => {
       return developer.available === true;
@@ -61,7 +88,7 @@ class App extends React.Component {
       <div className="App">
         <div className="container">
           <Header />
-          <AddDeveloper />
+          <AddDeveloper addNewDevFunc={this.addNewDeveloper} />
           <DeveloperCount count={availableDevelopers.length} />
           <h2>Available right now:</h2>
           {availableDevelopers.map(developer => {
